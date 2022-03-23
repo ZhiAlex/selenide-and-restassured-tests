216e68463a9df14fd06a72922bd49706e87e0c2d
Alex <zhialex.ru@gmail.com>
2022-03-23 23:02:08 +0300

    add allure

--- src/test/java/com/zhialex/tests/BaseTest.java
+++ src/test/java/com/zhialex/tests/BaseTest.java
@@ -1,8 +1,10 @@
 package com.zhialex.tests;
 
+import com.codeborne.selenide.Configuration;
 import com.zhialex.pages.CartPage;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.zhialex.helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.given;
@@ -25,5 +27,21 @@ public class BaseTest {
     @BeforeAll
     public static void beforeAll() {
         RestAssured.filters(withCustomTemplates());
+        String[] browser = System.getProperty("browser").split("_");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.browser = browser[0];
+        Configuration.browserVersion = browser[1];
+        Configuration.remote = String.format(
+                "https://%s:%s@selenoid.autotests.cloud/wd/hub",
+                System.getProperty("user"),
+                System.getProperty("password")
+        );
+        Configuration.holdBrowserOpen = false;
+        Configuration.browserSize = "1920x1080";
     }
 }
